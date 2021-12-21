66f2cbf19dd7939d3cb29e7511b866d4d4448c38
Shaik Feroz Basha <sbasha@delixus.com>
2021-12-21 21:38:20 +0530

    new modified files

---
+++ mythscript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Export Environment Variables
+myth=~/.local/bin
+
+#Executes the myth help command
+myth --help
+
+#Executes the myth scan command
+myth a modifier_reentrancy.sol
+
+
