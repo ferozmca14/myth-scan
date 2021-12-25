b4d715124e8d66e8191c5859b87bf5c0277bb656
Shaik Feroz Basha <sbasha@delixus.com>
2021-12-25 11:53:59 +0530

    script modified

--- scripts/mythscript.sh
+++ scripts/mythscript.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #Export Environment Variables
-myth=~/.local/bin
+export PATH="/home/delixus/.local/bin:$PATH"
 
 #Executes the myth help command
 myth --help
 
 #Executes the myth scan command
-myth a modifier_reentrancy.sol
+myth a ../modifier_reentrancy.sol
 
 
